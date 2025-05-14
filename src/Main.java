38ec5302c1d5eb1bacd0f473327326d25b1e15ac
swayam-21max <swmktria@gmail.com>
2025-05-14 09:46:24 +0530

    yes

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,8 @@ public class Main {
             System.out.printf("%-5s %-25s %-5s %-25s%n", "3.", "Create Account", "4.", "Deposit");
             System.out.printf("%-5s %-25s %-5s %-25s%n", "5.", "Withdraw", "6.", "Transfer");
             System.out.printf("%-5s %-25s %-5s %-25s%n", "7.", "Show Balance", "8.", "Transaction History");
-            System.out.printf("%-5s %-25s %-5s %-25s%n", "9.", "Logout", "10.", "Exit");
-            System.out.printf("%-5s %-25s%n", "11.", "List of Account Holders");
+            System.out.printf("%-5s %-25s %-5s %-25s%n", "9.", "Logout", "10.", "List of Account Holders");
+            System.out.printf("%-5s %-25s%n", "11.", "Search By Account ID" , "12.", "Exit");
 
             System.out.println("===========================================");
             System.out.print("Choose an option: ");
